c2e70d3bdd2104487410ee8b62934bba2e2e7dc9
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2022-01-11 17:45:04 -0500

    commit

---
+++ src/data/heroes.js
@@ -0,0 +1,182 @@
+export const heroes = [
+    {
+        'id': 'dc-batman',
+        'superhero':'Batman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Bruce Wayne',
+        'first_appearance':'Detective Comics #27',
+        'characters':'Bruce Wayne',
+        'img':'https://user-images.githubusercontent.com/87577172/149032913-b2de5d29-e50f-454d-9c62-ff2f26ecedd8.jpg'
+    },
+    {
+        'id': 'dc-superman',
+        'superhero':'Superman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Kal-El',
+        'first_appearance':'Action Comics #1',
+        'characters':'Kal-El',
+        'img':'https://user-images.githubusercontent.com/87577172/149032032-c0013a16-5d1c-4ff9-b1d8-7a883ded47e5.jpg'
+    },
+    {
+        'id': 'dc-flash',
+        'superhero':'Flash', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Jay Garrick',
+        'first_appearance':'Flash Comics #1',
+        'characters':'Jay Garrick, Barry Allen, Wally West, Bart Allen',
+        'img':'https://user-images.githubusercontent.com/87577172/149031680-55e9a265-ae3d-4c6f-89dc-c597c18aeef6.jpg'
+    },
+    {
+        'id': 'dc-green',
+        'superhero':'Green Lantern', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Alan Scott',
+        'first_appearance':'All-American Comics #16',
+        'characters':'Alan Scott, Hal Jordan, Guy Gardner, John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz',
+        'img':'https://user-images.githubusercontent.com/87577172/149031767-52339707-fe08-45cd-b92d-f63c0e0dcf50.jpg'
+    },
+    {
+        'id': 'dc-arrow',
+        'superhero':'Green Arrow', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Oliver Queen',
+        'first_appearance':'More Fun Comics #73',
+        'characters':'Oliver Queen',
+        'img':'https://user-images.githubusercontent.com/87577172/149029429-f676364e-3be1-44c4-b0a8-360fa94df84f.jpg'
+    },
+    {
+        'id': 'dc-wonder',
+        'superhero':'Wonder Woman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Princess Diana',
+        'first_appearance':'All Star Comics #8',
+        'characters':'Princess Diana',
+        'img':'https://user-images.githubusercontent.com/87577172/149032109-40833d56-161a-4fc4-9f39-6a5fb67daa12.jpg'
+    },
+    {
+        'id': 'dc-martian',
+        'superhero':'Martian Manhunter', 
+        'publisher':'DC Comics', 
+        'alter_ego':'J\'onn J\'onzz',
+        'first_appearance':'Detective Comics #225',
+        'characters':'Martian Manhunter',
+        'img':'https://user-images.githubusercontent.com/87577172/149031834-863073cc-1323-4a5f-b3f6-b4d94624bfc7.jpg'
+    },
+    {
+        'id': 'dc-robin',
+        'superhero':'Robin/Nightwing', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dick Grayson',
+        'first_appearance':'Detective Comics #38',
+        'characters':'Dick Grayson',
+        'img':'https://user-images.githubusercontent.com/87577172/149031912-eb4f354a-d406-457a-ac47-40031e04a689.jpg'
+    },
+    {
+        'id': 'dc-blue',
+        'superhero':'Blue Beetle', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dan Garret',
+        'first_appearance':'Mystery Men Comics #1',
+        'characters':'Dan Garret, Ted Kord, Jaime Reyes',
+        'img': 'https://user-images.githubusercontent.com/87577172/149031506-8ad661b0-0490-48c7-bf52-d838246733cd.jpg'
+    },
+    {
+        'id': 'dc-black',
+        'superhero':'Black Canary', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dinah Drake',
+        'first_appearance':'Flash Comics #86',
+        'characters':'Dinah Drake, Dinah Lance',
+        'img' : 'https://user-images.githubusercontent.com/87577172/149031380-3abff335-9f4b-4a5e-acc4-c3e768ab367c.jpg'
+    },
+    {
+        'id': 'marvel-spider',
+        'superhero':'Spider Man', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Peter Parker',
+        'first_appearance':'Amazing Fantasy #15',
+        'characters':'Peter Parker',
+        'img':'https://user-images.githubusercontent.com/87577172/149032645-e51e01af-d2f8-42aa-8ded-cde4bab49ce3.jpg'
+    },
+    {
+        'id': 'marvel-captain',
+        'superhero':'Captain America', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Steve Rogers',
+        'first_appearance':'Captain America Comics #1',
+        'characters':'Steve Rogers',
+        'img':'https://user-images.githubusercontent.com/87577172/149032182-b4212764-7f07-4fb9-b2f7-3a94acba65ac.jpg'
+    },
+    {
+        'id': 'marvel-iron',
+        'superhero':'Iron Man', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Tony Stark',
+        'first_appearance':'Tales of Suspense #39',
+        'characters':'Tony Stark',
+        'img':'https://user-images.githubusercontent.com/87577172/149032525-7ba31ad9-b37d-4471-a3ed-9afe5e9629a0.jpg'
+    },
+    {
+        'id': 'marvel-thor',
+        'superhero':'Thor', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Thor Odinson',
+        'first_appearance':'Journey into Myster #83',
+        'characters':'Thor Odinson',
+        'img':'https://user-images.githubusercontent.com/87577172/149032715-773122fa-ddf3-4873-b09f-07e1fd1e7b71.jpg'
+    },
+    {
+        'id': 'marvel-hulk',
+        'superhero':'Hulk', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Bruce Banner',
+        'first_appearance':'The Incredible Hulk #1',
+        'characters':'Bruce Banner',
+        'img':'https://user-images.githubusercontent.com/87577172/149032482-0baed661-ae49-43a5-8812-e9a4d7d0f3ca.jpg'
+    },
+    {
+        'id': 'marvel-wolverine',
+        'superhero':'Wolverine', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'James Howlett',
+        'first_appearance':'The Incredible Hulk #180',
+        'characters':'James Howlett',
+        'img':'https://user-images.githubusercontent.com/87577172/149032770-cf14633f-9407-4807-9ea3-aaf9342c3b8c.jpg'
+    },
+    {
+        'id': 'marvel-daredevil',
+        'superhero':'Daredevil', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Matthew Michael Murdock',
+        'first_appearance':'Daredevil #1',
+        'characters':'Matthew Michael Murdock',
+        'img':'https://user-images.githubusercontent.com/87577172/149032330-af8fd978-73f4-4c08-8d6c-ddf4ed6718af.jpg'
+    },
+    {
+        'id': 'marvel-hawkeye',
+        'superhero':'Hawkeye', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Clinton Francis Barton',
+        'first_appearance':'Tales of Suspense #57',
+        'characters':'Clinton Francis Barton',
+        'img':'https://user-images.githubusercontent.com/87577172/149032414-8f53504e-5b45-4f6d-a532-ac63febe73d3.jpg'
+    },
+    {
+        'id': 'marvel-cyclops',
+        'superhero':'Cyclops', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Scott Summers',
+        'first_appearance':'X-Men #1',
+        'characters':'Scott Summers',
+        'img':'https://user-images.githubusercontent.com/87577172/149032255-219069f8-572f-40fa-8efb-e1a3f9177a8e.jpg'
+    },
+    {
+        'id': 'marvel-silver',
+        'superhero':'Silver Surfer', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Norrin Radd',
+        'first_appearance':'The Fantastic Four #48',
+        'characters':'Norrin Radd',
+        'img':'https://user-images.githubusercontent.com/87577172/149032575-adc5a06e-55a0-47cc-87c4-751b38eec68c.jpg'
+    }
+]
